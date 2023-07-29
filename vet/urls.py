c0f24756e6af803cc2ccf8871901655e9a430244
AnuarShavkilov <anuar1910q@gmail.com>
2023-07-29 12:56:10 +0300

    redirect

--- vet/urls.py
+++ vet/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('<price_param>', views.get_price),
+    path('<int:price_param>', views.get_price_by_num),
+    path('<str:price_param>', views.get_price),
 ]
--- vet/views.py
+++ vet/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 price_dict = {
     'dermatolog': 'Цена приема дерматолога - 1900р',
@@ -10,9 +10,19 @@ price_dict = {
     'vacine': 'Цена вакцинации - 1000р',
 }
 
+
 def get_price(request, price_param):
     description = price_dict.get(price_param)
     if description:
         return HttpResponse(description)
     else:
         return HttpResponseNotFound(f'Нет такой цены - {price_param}')
+
+
+def get_price_by_num(request, price_param):
+    price_list = list(price_dict)
+    if price_param>len(price_list):
+        return HttpResponseNotFound(f'Нет такой цены - {price_param}')
+    name_price = price_list[price_param-1]
+    return HttpResponseRedirect(f'/price/{name_price}')
+
