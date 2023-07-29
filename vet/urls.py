bfd20349146bbf7d56833fc17ab88c2dd1d575a1
AnuarShavkilov <anuar1910q@gmail.com>
2023-07-29 13:44:17 +0300

    main menu

--- vet/urls.py
+++ vet/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('<int:price_param>', views.get_price_by_num),
-    path('<str:price_param>', views.get_price),
+    path('<str:price_param>', views.get_price, name='price-name'),
 ]
--- vet/views.py
+++ vet/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 price_dict = {
     'dermatolog': 'Цена приема дерматолога - 1900р',
@@ -11,18 +12,32 @@ price_dict = {
 }
 
 
+def index(request):
+    price_list = list(price_dict)
+    index_list = ''
+    for sign in price_list:
+        reversed_path = reverse('price-name', args=[sign, ])
+        index_list += f'<li> <a href = {reversed_path}>{sign.title()}</a> </li>'
+    responce = f"""
+    <ul>
+        {index_list}
+    </ul>
+    """
+    return HttpResponse(responce)
+
+
 def get_price(request, price_param):
     description = price_dict.get(price_param)
     if description:
-        return HttpResponse(description)
+        return HttpResponse(f'<h2>{description}<h2>')
     else:
         return HttpResponseNotFound(f'Нет такой цены - {price_param}')
 
 
 def get_price_by_num(request, price_param):
     price_list = list(price_dict)
-    if price_param>len(price_list):
+    if price_param > len(price_list):
         return HttpResponseNotFound(f'Нет такой цены - {price_param}')
-    name_price = price_list[price_param-1]
-    return HttpResponseRedirect(f'/price/{name_price}')
-
+    name_price = price_list[price_param - 1]
+    reversed_url = reverse('price-name', args=[name_price, ])
+    return HttpResponseRedirect(reversed_url)
