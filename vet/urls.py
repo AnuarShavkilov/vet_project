8b32b3dd5dfa0c99dad37a04387685e3cc06e7ef
anuar1910q@gmail.com <Ppoe0132es>
2023-07-30 14:59:37 +0300

    first task

--- vet/urls.py
+++ vet/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index_price),
+    path('type', views.index_type),
+    path('type/<str:price_type>', views.get_type, name='type-name'),
     path('<int:price_param>', views.get_price_by_num),
     path('<str:price_param>', views.get_price, name='price-name'),
 ]
--- vet/views.py
+++ vet/views.py
@@ -11,8 +11,14 @@ price_dict = {
     'vacine': 'Цена вакцинации - 1000р',
 }
 
+type_dict = {
+    'lechenie': ['dermatolog', 'hirurg', 'okulist'],
+    'terapiya': ['terapevt', 'vacine'],
+    'krasota': ['grummer', ]
+}
 
-def index(request):
+
+def index_price(request):
     price_list = list(price_dict)
     index_list = ''
     for sign in price_list:
@@ -24,6 +30,38 @@ def index(request):
     </ul>
     """
     return HttpResponse(responce)
+
+
+def index_type(request):
+    price_type_list = list(type_dict)
+    index_list = ''
+    for sign in price_type_list:
+        reversed_path = reverse('type-name', args=[sign, ])
+        index_list += f'<li> <a href = {reversed_path}>{sign.title()}</a> </li>'
+    responce = f"""
+        <ul>
+            {index_list}
+        </ul>
+        """
+    return HttpResponse(responce)
+
+
+def get_type(request, price_type):
+    reversed_url = reverse('type-name', args=[price_type, ])
+    description = type_dict.get(price_type)
+    index_list = ''
+    for sign in description:
+        reversed_path = reverse('price-name', args=[sign, ])
+        index_list += f'<li> <a href = {reversed_path}>{sign.title()}</a> </li>'
+    responce = f"""
+        <ul>
+            {index_list}
+        </ul>
+        """
+    if reversed_url:
+        return HttpResponse({responce})
+    else:
+        return HttpResponseNotFound(f'Нет такого - {price_type}')
 
 
 def get_price(request, price_param):
