247f5e088e544386480df8cf3dcc1bd5f4f1b88b
AnuarShavkilov <anuar1910q@gmail.com>
2023-08-03 17:07:52 +0300

    css

--- vet/urls.py
+++ vet/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_price),
-    path('type', views.index_type),
+    path('', views.index_price, name = 'main'),
+    path('type', views.index_type, name = 'types'),
     path('type/<str:price_type>', views.get_type, name='type-name'),
     path('<int:price_param>', views.get_price_by_num),
     path('<str:price_param>', views.get_price, name='price-name'),
